209db2f724ac5054f9b6c677390f0e0cc85f2b8e
lindx <linder.igor@gmail.com>
2021-04-18 06:32:54 +0300

    Dont do it in real life

---
+++ public/reader.js
@@ -0,0 +1,7 @@
+// @ts-check
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+
+// BEGIN (write your solution here)
+console.log(myvar);
+// END
---
+++ public/writer.js
@@ -0,0 +1,5 @@
+// @ts-check
+
+// BEGIN (write your solution here)
+window.myvar = 'Dont do it in real life';
+// END
