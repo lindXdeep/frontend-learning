f58621caf74bf0c921c5e23214225006796fec79
lindx <linder.igor@gmail.com>
2021-04-21 21:48:30 +0300

    simple modal window

---
+++ static/modal.js
@@ -0,0 +1,13 @@
+let modal = document.getElementById('myModal');
+let btn = document.getElementById('mybutton');
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = () => modal.style.display = "block";
+
+span.onclick = () => modal.style.display = "none";
+
+window.onclick = (event) => {
+  if (event.target == modal) {
+    modal.style.display = "none"
+  }
+} 
