9c0462b571ba8efae58b16d63cc288bbcd20a21c
lindx <linder.igor@gmail.com>
2021-04-23 01:49:26 +0300

    modal-1

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 
 let modal = document.getElementById('myModal');
-let btn = document.getElementById('myBtn');
 
-let span = document.getElementsByClassName("close")[0];
+let btn = document.getElementById('myButton');
+let span = document.getElementsByClassName('modal-close')[0];
 
 btn.onclick = () => modal.style.display = "block";
 span.onclick = () => modal.style.display = "none";
-
-// Смотрим на окне события,  если событие произошлол с id=modal
-// тогда скрываем блок
-
-window.onclick = (event) => {
-  if (event.target == modal) {
-    modal.style.display ="none";
-  }
-}
