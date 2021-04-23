4d99eb1d09cc04d955dd2d964519af4c8345ccff
lindx <linder.igor@gmail.com>
2021-04-23 03:32:18 +0300

    modal-2

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ let btn = document.getElementById('myButton');
 let span = document.getElementsByClassName('modal-close')[0];
 
 btn.onclick = () => modal.style.display = "block";
+
 span.onclick = () => modal.style.display = "none";
+
+
+window.onclick = (event) => {
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
