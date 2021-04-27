f9ae1162380a1c403334a4716a51449885443ccd
lindx <linder.igor@gmail.com>
2021-04-27 13:27:34 +0300

    add controllers and pages

--- db.sql
+++ db.sql
@@ -4,3 +4,19 @@ create database modal;
 SHOW DATABASES;
 USE modal;
 SHOW TABLES;
+
+
+INSERT INTO modal.user(id, username, lastname)
+  VALUES(1,'Tom', 'johson');
+
+INSERT INTO modal.user(id, username, lastname)  
+  VALUES(2, 'Bob', 'wilson');
+
+INSERT INTO modal.user(id, username, lastname)  
+  VALUES(3, 'joe', 'qwfgew');
+
+INSERT INTO modal.user(id, username, lastname)  
+  VALUES(4, 'Lue', 'sddsa');
+
+INSERT INTO modal.user(id, username, lastname)  
+  VALUES(5, 'Zoe', 'dfgdf');
--- src/main/java/win/modal/controllers/MainController.java
+++ src/main/java/win/modal/controllers/MainController.java
@@ -1,22 +1,62 @@
 package win.modal.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import win.modal.entities.User;
 import win.modal.repositories.UserRepository;
-
-@RequiredArgsConstructor
 
 @Controller
 public class MainController {
-  
-  private UserRepository userRepository;
+
+  private final UserRepository userRepository;
+
+  @Autowired
+  public MainController(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
 
   @GetMapping("/")
-  public String main(final ModelMap modelMap){
-        
+  public String main(final ModelMap modelMap, @RequestParam(defaultValue = "0") int page) {
+
+    modelMap.addAttribute("data", userRepository.findAll(PageRequest.of(page, 4)));
+    modelMap.addAttribute("currentPage", page);
+
     return "index";
+  }
+
+  @PostMapping("/save")
+  public String save(final User user) {
+
+    userRepository.save(user);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/delete")
+  public String delete(final Long id) {
+
+    userRepository.deleteById(id);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("/findOne")
+  @ResponseBody
+  public User findOne(final Long id) {
+
+    return userRepository.findById(id).get();
   }
 }
