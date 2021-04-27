c91e132344a35479711f9183e8c98826a348db66
lindx <linder.igor@gmail.com>
2021-04-27 11:21:40 +0300

    Entity

---
+++ src/main/java/win/modal/App.java
@@ -0,0 +1,15 @@
+package win.modal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * App
+ */
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
