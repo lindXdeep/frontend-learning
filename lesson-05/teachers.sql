2d15d6f63c4a741714cb46b90e5d621eee47748d
lindx <linder.igor@gmail.com>
2021-10-12 01:55:10 +0300

    4. Найти потоки преподавателя Натальи Петровой а также потоки, по которым успеваемость ниже 4.8

--- lesson-05/teachers.sql
+++ lesson-05/teachers.sql
@@ -49,19 +49,25 @@ performance
 -- 3. Найти среднюю успеваемость всех потоков преподавателя Николая Савельева. 
 -- В отчёт вывести идентификатор преподавателя и среднюю оценку по потокам.
 
-  SELECT 
-    teacher_id,
-    AVG(performance) 
-      FROM achievements
-        WHERE 
-          teacher_id = (SELECT id FROM teachers WHERE name = 'Николай' AND surname = 'Савельев');
-      
--- 4. Найти потоки преподавателя Натальи Петровой а также потоки, по которым успеваемость ниже 4.8. 
+SELECT 
+  teacher_id,
+  AVG(performance) 
+    FROM achievements
+      WHERE 
+        teacher_id = (SELECT id FROM teachers WHERE name = 'Николай' AND surname = 'Савельев');
+    
+-- .4. Найти потоки преподавателя Натальи Петровой а также потоки, по которым успеваемость ниже 4.8 
 -- В отчёт вывести идентификатор потока, фамилию и имя преподавателя.
 
-
-
-
-
+SELECT 
+  stream_id,
+  teacher 
+    FROM(
+      SELECT 
+        (SELECT name || ' ' || surname FROM teachers WHERE teachers.id = achievements.teacher_id) AS teacher, 
+        stream_id 
+          FROM achievements
+            WHERE performance < 4.8)
+            ;
 
 
