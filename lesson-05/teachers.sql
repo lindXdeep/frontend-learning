d91f56fd87823d5c3c1ad34b4113c0106c18fccf
lindx <linder.igor@gmail.com>
2021-10-12 02:44:13 +0300

    Найти два потока с самыми низкими оценками за успеваемость.

--- lesson-05/teachers.sql
+++ lesson-05/teachers.sql
@@ -11,40 +11,21 @@ SELECT
 -- 2. Найти два потока с самыми низкими оценками за успеваемость. 
 -- В отчёт вывести номер потока, название курса, фамилию и имя преподавателя (одним столбцом), оценку успеваемости.
 
-
-SELECT (name || ' ' || surname) AS teacher FROM teachers;
-
-SELECT 
-  stream_id,
-  (SELECT (name || ' ' || surname) FROM teachers WHERE teachers.id = achievements.teacher_id) AS teacher,
-  achievements.performance
-    FROM achievements
-
---
-
-SELECT 
-  training_groups.number,
-
-  (SELECT name FROM courses WHERE courses.id = training_groups.course_id) AS course_name
-    FROM training_groups;
-
---
-
-SELECT 
-teacher,
-performance
-
- FROM 
-
-(
-  SELECT 
-    stream_id,
-    (SELECT (name || ' ' || surname) FROM teachers WHERE teachers.id = achievements.teacher_id) AS teacher,
-    achievements.performance
-      FROM achievements
-)
-
-
+SELECT
+  number,
+  (SELECT name FROM courses WHERE courses.id = course_id) AS course_name,
+  teachers,
+  performance
+    FROM (
+      SELECT 
+        (SELECT number FROM training_groups WHERE training_groups.id = stream_id) AS number,
+        (SELECT course_id FROM training_groups WHERE training_groups.id = stream_id) AS course_id,
+        (SELECT (name || ' ' || surname) FROM teachers WHERE teachers.id = teacher_id) AS teachers,
+        performance 
+          FROM achievements
+      )
+        ORDER BY performance ASC
+        LIMIT 2;
 
 -- 3. Найти среднюю успеваемость всех потоков преподавателя Николая Савельева. 
 -- В отчёт вывести идентификатор преподавателя и среднюю оценку по потокам.
@@ -56,7 +37,7 @@ SELECT
       WHERE 
         teacher_id = (SELECT id FROM teachers WHERE name = 'Николай' AND surname = 'Савельев');
     
--- .4. Найти потоки преподавателя Натальи Петровой а также потоки, по которым успеваемость ниже 4.8 
+-- 4. Найти потоки преподавателя Натальи Петровой а также потоки, по которым успеваемость ниже 4.8 
 -- В отчёт вывести идентификатор потока, фамилию и имя преподавателя.
 
 SELECT 
