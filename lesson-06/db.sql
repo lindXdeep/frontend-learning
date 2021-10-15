2b3385d0b24879cf4dd5578e9a6b238d2802cd74
lindx <linder.igor@gmail.com>
2021-10-16 00:18:39 +0300

    Найдите общее количество учеников для каждого курса.

--- lesson-06/db.sql
+++ lesson-06/db.sql
@@ -1,12 +1,22 @@
+-- 1. Покажите информацию по потокам. 
+-- В отчет выведите номер потока, название курса и дату начала занятий.
 
--- 1. Покажите информацию по потокам. В отчет выведите номер потока, название курса и дату начала занятий.
-SELECT
+SELECT 
   training_groups.number,
   courses.name,
   training_groups.started_at
     FROM training_groups
-      INNER JOIN courses
-        ON training_groups.course_id = courses.id;
+      INNER JOIN courses ON training_groups.course_id = courses.id;
 
+-- 2. Найдите общее количество учеников для каждого курса. 
+-- В отчёт выведите название курса и количество учеников по всем потокам курса.
 
-  
+SELECT 
+  courses.name,
+  SUM(training_groups.students_amount) AS students_amount
+    FROM courses
+      JOIN training_groups
+        ON courses.id = training_groups.course_id
+          GROUP BY courses.name;
+       
+       
