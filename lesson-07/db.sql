075badc1b5cddd99813cbdffee81384964089aeb
lindx <linder.igor@gmail.com>
2021-10-16 22:46:44 +0300

    Создайте триггер для таблицы успеваемости,

--- lesson-07/db.sql
+++ lesson-07/db.sql
@@ -24,14 +24,38 @@ SELECT
 -- 2. Удалите из базы данных всю информацию, 
 --    которая относится к преподавателю с идентификатором, равным 3. Используйте транзакцию.
 
+BEGIN TRANSACTION;
+  DELETE FROM training_groups 
+    WHERE id = (SELECT stream_id FROM achievements WHERE teacher_id = 3);
+  DELETE FROM achievements
+    WHERE teacher_id = 3;
+  DELETE FROM teachers
+    WHERE id = 3;
+COMMIT;
 
+-- 3. Создайте триггер для таблицы успеваемости, который проверяет 
+--    значение успеваемости на соответствие диапазону чисел от 0 до 5 включительно.
 
--- 3. Создайте триггер для таблицы успеваемости, 
---    который проверяет значение успеваемости на соответствие диапазону чисел от 0 до 5 включительно.
+CREATE TRIGGER check_performance 
+  BEFORE INSERT
+    ON achievements
+  BEGIN
+    SELECT CASE
+    WHEN
+      (NEW.performance NOT BETWEEN 0.0 AND 5.0)
+    THEN
+      RAISE(ABORT, 'Wrong value for performance!')
+    END;
+  END;
 
-
+INSERT INTO achievements (teacher_id, stream_id, performance)
+  VALUES(11, 11, 11);
 
 -- 4. Дополнительное задание. 
 --    Создайте триггер для таблицы потоков, который проверяет, что дата начала потока больше текущей даты, 
 --    а номер потока имеет наибольшее значение среди существующих номеров. 
 --    При невыполнении условий необходимо вызвать ошибку с информативным сообщением.
+
+
+
+
